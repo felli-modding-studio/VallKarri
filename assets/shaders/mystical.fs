7676e4383d86de9c0f4a90f15ec8ab52781f6056
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-12-14 15:38:16 +0000

    carry over sounds and shaders

---
+++ assets/shaders/mystical.fs
@@ -0,0 +1,179 @@
+#if defined(VERTEX) || __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
+    #define PRECISION highp
+#else
+    #define PRECISION mediump
+#endif
+
+// !! change this variable name to your Shader's name
+// YOU MUST USE THIS VARIABLE IN THE vec4 effect AT LEAST ONCE
+
+// Values of this variable:
+// self.ARGS.send_to_shader[1] = math.min(self.VT.r*3, 1) + (math.sin(G.TIMERS.REAL/28) + 1) + (self.juice and self.juice.r*20 or 0) + self.tilt_var.amt
+// self.ARGS.send_to_shader[2] = G.TIMERS.REAL
+extern PRECISION vec2 mystical;
+
+extern PRECISION number dissolve;
+extern PRECISION number time;
+// [Note] sprite_pos_x _y is not a pixel position!
+//        To get pixel position, you need to multiply  
+//        it by sprite_width _height (look flipped.fs)
+// (sprite_pos_x, sprite_pos_y, sprite_width, sprite_height) [not normalized]
+extern PRECISION vec4 texture_details;
+// (width, height) for atlas texture [not normalized]
+extern PRECISION vec2 image_details;
+extern bool shadow;
+extern PRECISION vec4 burn_colour_1;
+extern PRECISION vec4 burn_colour_2;
+
+// [Required] 
+// Apply dissolve effect (when card is being "burnt", e.g. when consumable is used)
+vec4 dissolve_mask(vec4 tex, vec2 texture_coords, vec2 uv);
+
+number hue(number s, number t, number h)
+{
+	number hs = mod(h, 1.)*6.;
+	if (hs < 1.) return (t-s) * hs + s;
+	if (hs < 3.) return t;
+	if (hs < 4.) return (t-s) * (4.-hs) + s;
+	return s;
+}
+
+vec4 RGB(vec4 c)
+{
+	if (c.y < 0.0001)
+		return vec4(vec3(c.z), c.a);
+
+	number t = (c.z < .5) ? c.y*c.z + c.z : -c.y*c.z + (c.y+c.z);
+	number s = 2.0 * c.z - t;
+	return vec4(hue(s,t,c.x + 1./3.), hue(s,t,c.x), hue(s,t,c.x - 1./3.), c.w);
+}
+
+vec4 HSL(vec4 c)
+{
+	number low = min(c.r, min(c.g, c.b));
+	number high = max(c.r, max(c.g, c.b));
+	number delta = high - low;
+	number sum = high+low;
+
+	vec4 hsl = vec4(.0, .0, .5 * sum, c.a);
+	if (delta == .0)
+		return hsl;
+
+	hsl.y = (hsl.z < .5) ? delta / sum : delta / (2.0 - sum);
+
+	if (high == c.r)
+		hsl.x = (c.g - c.b) / delta;
+	else if (high == c.g)
+		hsl.x = (c.b - c.r) / delta + 2.0;
+	else
+		hsl.x = (c.r - c.g) / delta + 4.0;
+
+	hsl.x = mod(hsl.x / 6., 1.);
+	return hsl;
+}
+
+float modify(float a) {
+    return a/1.2;
+}
+
+float freaky_sin(float a) {
+    return sin(modify(a));
+}
+
+float freaky_cos(float a) {
+    return sin(modify(a));
+}
+
+vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords )
+{
+    vec4 tex = Texel( texture, texture_coords);
+    vec2 uv = (((texture_coords)*(image_details)) - texture_details.xy*texture_details.ba)/texture_details.ba;
+    vec2 adjusted_uv = uv - vec2(0.5, 0.5);
+    adjusted_uv.x = adjusted_uv.x*texture_details.b/texture_details.a;
+
+    number low = min(tex.r, min(tex.g, tex.b));
+    number high = max(tex.r, max(tex.g, tex.b));
+	number delta = min(high, max(0.5, 1. - low));
+
+    number fac = max(min(2.*freaky_sin((length(90.*adjusted_uv) + mystical.r*2.) + 3.*(1.+0.8*freaky_cos(length(113.1121*adjusted_uv) - mystical.r*3.121))) - 1. - max(5.-length(90.*adjusted_uv), 0.), 1.), 0.);
+    vec2 rotater = vec2(freaky_cos(mystical.r*0.1221), freaky_sin(mystical.r*0.3512));
+    number angle = dot(rotater, adjusted_uv)/(length(rotater)*length(adjusted_uv));
+    number fac2 = max(min(5.*freaky_cos(mystical.g*0.3 + angle*3.14*(2.2+0.9*freaky_sin(mystical.r*1.65 + 0.2*mystical.g))) - 4. - max(2.-length(20.*adjusted_uv), 0.), 1.), 0.);
+    number fac3 = 0.3*max(min(2.*freaky_sin(mystical.r*5. + uv.x*3. + 3.*(1.+0.5*freaky_cos(mystical.r*7.))) - 1., 1.), -1.);
+    number fac4 = 0.3*max(min(2.*freaky_sin(mystical.r*6.66 + uv.y*3.8 + 3.*(1.+0.5*freaky_cos(mystical.r*3.414))) - 1., 1.), -1.);
+
+    number maxfac = max(max(fac, max(fac2, max(fac3, max(fac4, 0.0)))) + 2.2*(fac+fac2+fac3+fac4), 0.);
+
+    vec3 goal_colour = vec3(0.9804*uv.y, 0.3647, 0.9804*uv.x);
+    goal_colour += 0.25;
+    float boost = 0.75;
+    float power = 0.5;
+
+    tex.rgb *= goal_colour;
+
+    tex.r = tex.r-delta + delta*maxfac*(pow(goal_colour.r+boost,power));
+    tex.g = tex.g-delta + delta*maxfac*(pow(goal_colour.g+boost,power));
+    tex.b = tex.b + delta*maxfac*(pow(goal_colour.b+boost,power));
+    // tex.a = min(tex.a, 0.3*tex.a + 0.9*min(0.5, maxfac*0.1));
+    tex.a = min(0.4, tex.a * 0.9);
+
+    return dissolve_mask(tex, texture_coords, uv);
+}
+
+vec4 dissolve_mask(vec4 tex, vec2 texture_coords, vec2 uv)
+{
+    if (dissolve < 0.001) {
+        return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, shadow ? tex.a*0.3: tex.a);
+    }
+
+    float adjusted_dissolve = (dissolve*dissolve*(3.-2.*dissolve))*1.02 - 0.01; //Adjusting 0.0-1.0 to fall to -0.1 - 1.1 scale so the mask does not pause at extreme values
+
+	float t = time * 10.0 + 2003.;
+	vec2 floored_uv = (floor((uv*texture_details.ba)))/max(texture_details.b, texture_details.a);
+    vec2 uv_scaled_centered = (floored_uv - 0.5) * 2.3 * max(texture_details.b, texture_details.a);
+	
+	vec2 field_part1 = uv_scaled_centered + 50.*vec2(sin(-t / 143.6340), cos(-t / 99.4324));
+	vec2 field_part2 = uv_scaled_centered + 50.*vec2(cos( t / 53.1532),  cos( t / 61.4532));
+	vec2 field_part3 = uv_scaled_centered + 50.*vec2(sin(-t / 87.53218), sin(-t / 49.0000));
+
+    float field = (1.+ (
+        cos(length(field_part1) / 19.483) + sin(length(field_part2) / 33.155) * cos(field_part2.y / 15.73) +
+        cos(length(field_part3) / 27.193) * sin(field_part3.x / 21.92) ))/2.;
+    vec2 borders = vec2(0.2, 0.8);
+
+    float res = (.5 + .5* cos( (adjusted_dissolve) / 82.612 + ( field + -.5 ) *3.14))
+    - (floored_uv.x > borders.y ? (floored_uv.x - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y > borders.y ? (floored_uv.y - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.x < borders.x ? (borders.x - floored_uv.x)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y < borders.x ? (borders.x - floored_uv.y)*(5. + 5.*dissolve) : 0.)*(dissolve);
+
+    if (tex.a > 0.01 && burn_colour_1.a > 0.01 && !shadow && res < adjusted_dissolve + 0.8*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+        if (!shadow && res < adjusted_dissolve + 0.5*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+            tex.rgba = burn_colour_1.rgba;
+        } else if (burn_colour_2.a > 0.01) {
+            tex.rgba = burn_colour_2.rgba;
+        }
+    }
+
+    return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, res > adjusted_dissolve ? (shadow ? tex.a*0.3: tex.a) : .0);
+}
+
+// for transforming the card while your mouse is on it
+extern PRECISION vec2 mouse_screen_pos;
+extern PRECISION float hovering;
+extern PRECISION float screen_scale;
+
+#ifdef VERTEX
+vec4 position( mat4 transform_projection, vec4 vertex_position )
+{
+    if (hovering <= 0.){
+        return transform_projection * vertex_position;
+    }
+    float mid_dist = length(vertex_position.xy - 0.5*love_ScreenSize.xy)/length(love_ScreenSize.xy);
+    vec2 mouse_offset = (vertex_position.xy - mouse_screen_pos.xy)/screen_scale;
+    float scale = 0.2*(-0.03 - 0.3*max(0., 0.3-mid_dist))
+                *hovering*(length(mouse_offset)*length(mouse_offset))/(2. -mid_dist);
+
+    return transform_projection * vertex_position + vec4(0,0,0,scale);
+}
+#endif
